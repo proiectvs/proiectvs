bcbb5bde9d34a3588cbba1b7799a961189cf2432
proiectvs <51097282+proiectvs@users.noreply.github.com>
2019-05-28 16:44:23 +0300

    Add files via upload

---
+++ ContosoUniversity/Controllers/DemoController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContosoUniversity.Controllers
+{
+    [MyLogActionFilter]
+    public class DemoController : Controller
+    {
+       
+       /* public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Demo
+     /*   public ActionResult Index()
+        {
+            return RedirectToAction("GetAllCustomers", "Process");
+        } 
+        public ActionResult Search(string name)
+        {
+            var input = Server.HtmlEncode(name);
+            return Content(input);
+        }
+	
+         [HttpGet]
+      public ActionResult Search(){
+         var input = "Another Search action";
+         return Content(input);  */
+      
+        [ActionName("CurrentTime")]
+        [OutputCache(Duration = 20)]
+        public string Time()
+        {
+            return TimeString();
+        }
+        [NonAction]
+        public string TimeString() { return "time is " + DateTime.Now.ToString("T"); } 
+    } 
+}
---
+++ ContosoUniversity/Controllers/ProcessController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContosoUniversity.Controllers
+{
+    public class ProcessController : Controller
+    {
+        // GET: Process
+        public string List()
+        {
+            return "Acesta e procesul";
+        }
+        public string GetAllCustomers()
+        {
+            return @"<ul>
+      <li>Ali Raza</li>
+      <li>Mark Upston</li>
+      <li>Allan Bommer</li>
+      <li>Greg Jerry</li>
+   </ul>";
+        }
+    }
+}
